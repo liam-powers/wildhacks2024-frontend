c06cc2abc31afa09acc533108928e5f9a81f3a06
liam-powers <95184359+liam-powers@users.noreply.github.com>
2024-04-06 18:13:35 -0500

    home progress

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,30 +5,24 @@ import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <div className="bg-[#000130] w-screen h-screen">
-      <NavBar/>
-      <div className="justify-center">
-        Parking Re-Imagined
-        <Link href="./about">
-          <Button text="How it Works" />
-          <Button text="Try it Here" />
-        </Link>
+    <div className="bg-[#000130] w-screen h-screen bg-[url('/homeBackground.webp')] bg-blend-overlay bg-opacity-50 bg-no-repeat">
+      <div className="w-[1024px]">
+        <NavBar />
+
+        <div className="justify-center">
+          Parking Re-Imagined
+          <Link href="./about">
+            <Button text="How it Works" />
+            <Button text="Try it Here" />
+          </Link>
+        </div>
       </div>
       <div>
+        <div> Our Cities </div>
         <div>
-          Our Cities
-        </div>
-        <div>
-          San Francisco
-          Chicago
-          New York City
-          Evanston
-          Boston
-          Atlanta
-          Houston
+          San Francisco Chicago New York City Evanston Boston Atlanta Houston
           Las Angeles
         </div>
-
       </div>
     </div>
   );
