dedf87021fd7a71ca770d716321a79d468311a22
benchaddha <bchaddha2@gmail.com>
2024-04-06 20:08:40 -0500

    Search Bar functionality, need to add tailwind

--- frontend/src/app/findaspot/page.js
+++ frontend/src/app/findaspot/page.js
@@ -1,8 +1,26 @@
+"use client";
+import './findaspot.css';
+import { useState } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../components/Button";
+import NavBar from "../components/NavBar";
+import Search from "../components/Search";
+import SearchBar from "../components/SearchBar"; // Change this line
+import { SearchResultsList } from '../components/SearchResultsList';
+
 export default function findaspot() {
-    // function stuff can go here
-    return (
-        <div>
-            <h1>Find a Spot</h1>
+
+    const [results, setResults] = useState([]);
+  return (
+    <div className="bg-[#000130] w-screen h-screen bg-[url('/searchbarBackground.jpg')] bg-cover bg-center bg-no-repeat">
+      <NavBar/>
+        <div className="searchbar-container">
+            <div>Where are you headed?</div>
+            <SearchBar setResults = {setResults}/>
+            <SearchResultsList results={results}/>
         </div>
-    );
-};
+    </div>
+  );
+}
